eaf781916b95f578874c50985c48dd9a6b743efa
sreedhar <sreedhar70755@gmail.com>
2023-11-17 21:42:25 +0530

    tf

---
+++ local.tf
@@ -0,0 +1,12 @@
+locals {
+  public_subnet_ids= [for k,v in lookup(lookup(module.subnets,"public",null ),"subnets",null) : v.id]
+  db_subnet_ids= [for k,v in lookup(lookup(module.subnets,"db",null ),"subnets",null) : v.id]
+  app_subnet_ids= [for k,v in lookup(lookup(module.subnets,"app",null ),"subnets",null) : v.id]
+  private_subnet_ids= concat(local.app_subnet_ids,local.db_subnet_ids)
+
+  public_route_ids= [for k,v in lookup(lookup(module.subnets,"app",null ),"route_table_ids",null) : v.id]
+  app_route_ids= [for k,v in lookup(lookup(module.subnets,"app",null ),"route_table_ids",null) : v.id]
+  db_route_ids= [for k,v in lookup(lookup(module.subnets,"app",null ),"route_table_ids",null) : v.id]
+  private_route_ids = concat(local.app_route_ids,local.db_route_ids)
+
+}
--- main.tf
+++ main.tf
@@ -28,17 +28,25 @@ resource "aws_route" "route" {
 }
 
 resource "aws_eip" "lb" {
-  for_each = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
-  domain   = "vpc"
+  count  = length(local.public_subnet_ids)
+  domain = "vpc"
 }
 
-resource "aws_nat_gateway" "example" {
-  for_each      = lookup(lookup(module.subnets, "public", null), "subnets", null)
+resource "aws_nat_gateway" "ngw" {
+  count      = length(local.public_subnet_ids)
 
-  allocation_id = lookup(lookup(aws_eip.lb,each.key,null ), "id",null)
-  subnet_id = each.value["id"]
+  allocation_id = element(aws_eip.lb.id,count.index )
+  subnet_id = element(local.public_subnet_ids,count.index )
 
   tags = {
-    Name = each.key
+    Name = "ngw"
   }
+}
+
+resource "aws_route" "route" {
+  count = length(local.private_subnet_ids)
+
+  route_table_id         = element(local.private_subnet_ids,count.index )
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id             = element(aws_nat_gateway.ngw.id,count.index )
 }
