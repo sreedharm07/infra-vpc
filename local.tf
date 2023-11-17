e46c62f0e79aeb632075dc7482d3f0f78df5a22c
sreedhar <sreedhar70755@gmail.com>
2023-11-17 22:12:54 +0530

    tf

--- local.tf
+++ local.tf
@@ -4,9 +4,9 @@ locals {
   app_subnet_ids= [for k,v in lookup(lookup(module.subnets,"app",null ),"subnets",null) : v.id]
   private_subnet_ids= concat(local.app_subnet_ids,local.db_subnet_ids)
 
-  public_route_ids= [for k,v in lookup(lookup(module.subnets,"app",null ),"route_table_ids",null) : v.id]
+  public_route_ids= [for k,v in lookup(lookup(module.subnets,"public",null ),"route_table_ids",null) : v.id]
   app_route_ids= [for k,v in lookup(lookup(module.subnets,"app",null ),"route_table_ids",null) : v.id]
-  db_route_ids= [for k,v in lookup(lookup(module.subnets,"app",null ),"route_table_ids",null) : v.id]
+  db_route_ids= [for k,v in lookup(lookup(module.subnets,"db",null ),"route_table_ids",null) : v.id]
   private_route_ids = concat(local.app_route_ids,local.db_route_ids)
 
 }
