153943a64720411fce29588bf255ec43c57919c9
sreedhar <sreedhar70755@gmail.com>
2023-11-17 17:20:25 +0530

    tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr
 }
+
+
+module "subnets" {
+  source     = "./module"
+  for_each   = var.subnets
+  subnets    = each.value
+  vpc_id     = aws_vpc.main.id
+  cidr_block = each.value["cidr"]
+  az= each.value["az"]
+}
---
+++ module/vars.tf
@@ -0,0 +1,4 @@
+variable "subnets" {}
+variable "vpc_id" {}
+variable "cidr_block" {}
+variable "az" {}
